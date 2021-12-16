47604157483300a828af5042fb686e5cfa5f014c
guest1 <dzhumanazarov_t@auca.kg>
2021-12-16 16:08:50 +0600

    z

--- app/src/main/java/kg/dani/shukuraliev_keneshbek_final/MainActivity.kt
+++ app/src/main/java/kg/dani/shukuraliev_keneshbek_final/MainActivity.kt
@@ -1,12 +1,18 @@
 package kg.dani.shukuraliev_keneshbek_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageView = findViewById<ImageView>(R.id.imageView4)
+        imageView.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
+        }
     }
 
 }
---
+++ app/src/main/java/kg/dani/shukuraliev_keneshbek_final/MainActivity2.kt
@@ -0,0 +1,11 @@
+package kg.dani.shukuraliev_keneshbek_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
